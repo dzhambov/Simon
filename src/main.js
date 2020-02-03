bc467bb019f16cd47b74a0b55f83a8aa4df84921
Student <example@example.com>
2020-02-03 15:49:35 -0800

    game logic is functional UI minimal

--- src/main.js
+++ src/main.js
@@ -16,12 +16,40 @@ function displaycolor(game, i){
 
 
 $(document).ready(function(){
-  var game = new Simon();
+  var game = new Simon();                        
    $('#start-game').submit(function(event){
      event.preventDefault();
       game.randColor();
       var i = 0;
       displaycolor(game, i);
-      console.log(game);
+      $('#start-game').addClass('no-display');
+      console.log(game)
    });
+   $('#blue').click(function(){
+     game.addInputColor('blue')
+     console.log(game)
+   })
+   $('#red').click(function(){
+    game.addInputColor('red')
+    console.log(game)
+  })
+  $('#green').click(function(){
+    game.addInputColor('green')
+    console.log(game)
+  })
+  $('#yellow').click(function(){
+    game.addInputColor('yellow')
+    console.log(game)
+  })
+  $('#check').click(function(){
+    console.log(game)
+    if(game.checkAnswer()){
+      alert('Right! keeps going!')
+      $('#start-game').removeClass('no-display');
+    } else{
+      alert('Wrong! press okay to try again')
+      location.reload(true);
+    }
+    game.clearUserInput()
+  })
 });
--- src/simon-logic.js
+++ src/simon-logic.js
@@ -7,4 +7,19 @@ Simon.prototype.randColor = function(){
   var randomIndex = Math.floor(Math.random()*4);
   this.colorArr.push(this.colors[randomIndex]);
 };
+Simon.prototype.checkAnswer = function(){
+  var colors = this.colorArr.join('')
+  var inColors = this.playerInputArr.join('')
+    if(colors===inColors ){
+      return true
+    } else{
+      return false
+    }
+}
+Simon.prototype.addInputColor = function(color){
+  this.playerInputArr.push(color)
+}
+Simon.prototype.clearUserInput =function(){
+  this.playerInputArr = []
+}
 
